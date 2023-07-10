070bd7a667e3ea317be5da4d2984ebb79b6ddf0f
MansiRavindraMohan <manasimohan29@gmail.com>
2023-07-10 13:10:12 +0530

    [Mansi]: Program for example of types of constructors

---
+++ Day3B_Assignment/ConstructorTypes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B_Assignment
+{
+    public class ConstructorTypes
+    {
+        //Default Constructor
+        public ConstructorTypes()
+        {
+            Console.WriteLine("This is example of default constructor");
+        }
+        //Parameterised Constructor
+        public int Id;
+        public string Name;
+        public ConstructorTypes(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+            Console.WriteLine("This is example of Parameterised constructor");
+            Console.WriteLine(name);
+            Console.WriteLine(id);
+        }
+        //Copy Constructor
+        public ConstructorTypes(ConstructorTypes other)
+        {
+            this.Id = other.Id;
+            this.Name = other.Name;
+            Console.WriteLine("This is example of Copy constructor");
+            Console.WriteLine(other.Name);
+            Console.WriteLine(other.Id);
+        }
+    }
+}
---
+++ Day3B_Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConstructorTypes typesConstructors = new ConstructorTypes();
+            ConstructorTypes types = new ConstructorTypes(145, "Mansi");
+            ConstructorTypes typesTwo = new ConstructorTypes(types);
+            Console.ReadLine();
+        }
+    }
+}
